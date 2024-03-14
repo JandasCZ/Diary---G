8d9955f098a3ec73df055153328acb8a381dc943
22ib11_jandourek <22ib11_jandourek@spsjc.cz>
2024-03-14 09:49:38 +0100

    Přidejte soubory projektu.

---
+++ WpfApp/AddRecordWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    public partial class AddRecordWindow : Window
+    {
+        public AddRecordWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string GetRecord()
+        {
+            string text = textBox.Text;
+            string date = datePicker.SelectedDate?.ToShortDateString();
+            return $"ZPRÁVA: {text} - DATUM: {date}";
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddRecord_Click(object sender, RoutedEventArgs e)
+        {
+            AddRecordWindow addRecordWindow = new AddRecordWindow();
+            if (addRecordWindow.ShowDialog() == true)
+            {
+                listBox.Items.Add(addRecordWindow.GetRecord());
+            }
+        }
+
+        private void DeleteRecord_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedItem != null)
+            {
+                listBox.Items.Remove(listBox.SelectedItem);
+            }
+        }
+    }
+}
